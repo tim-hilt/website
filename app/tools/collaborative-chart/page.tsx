396991b10703a27545b1c9e0d84b99ebee8d1973
Tim Hilt <timhilt@live.de>
2023-12-18 14:37:02 +0100

    fix: Strip unnecessary stuff

--- app/tools/collaborative-chart/page.tsx
+++ app/tools/collaborative-chart/page.tsx
@@ -1,41 +1,60 @@
 // TODO: Don't render whole page as client component
+// -> Maybe with Server-Actions?
 "use client";
 
 import * as d3 from "d3";
-import { FormEvent, useEffect, useRef, useState } from "react";
+import { FormEvent, useEffect, useLayoutEffect, useRef, useState } from "react";
 
-type LinePlotProps = {
-  data: number[];
-  width?: number;
-  height?: number;
-  marginTop?: number;
-  marginRight?: number;
-  marginBottom?: number;
-  marginLeft?: number;
+type Point = {
+  x: number;
+  y: number;
 };
 
-// TODO: Make sense of the function
-function LinePlot({
-  data = [],
-  width = 640,
-  height = 400,
-  marginTop = 20,
-  marginRight = 20,
-  marginBottom = 30,
-  marginLeft = 40,
-}: LinePlotProps) {
-  const gx = useRef<SVGSVGElement>(null);
-  const gy = useRef<SVGSVGElement>(null);
+const useDimensions = (targetRef: React.RefObject<HTMLDivElement>) => {
+  const getDimensions = () => {
+    return {
+      width: targetRef.current ? targetRef.current.offsetWidth : 0,
+      height: targetRef.current ? targetRef.current.offsetHeight : 0,
+    };
+  };
 
-  const x = d3.scaleLinear(
-    [0, data.length - 1],
-    [marginLeft, width - marginRight]
-  );
-  const [minY, maxY] = d3.extent(data) as [number, number];
+  const [dimensions, setDimensions] = useState(getDimensions);
+
+  const handleResize = () => {
+    setDimensions(getDimensions());
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useLayoutEffect(() => {
+    handleResize();
+  }, []);
+
+  return dimensions;
+};
+
+type ScatterPlotProps = {
+  points: Array<Point>;
+  width: number;
+  height: number;
+};
+
+function ScatterPlot({ points, width, height }: ScatterPlotProps) {
+  const marginTop = 20;
+  const marginRight = 40;
+  const marginBottom = 20;
+  const marginLeft = 40;
+
+  const [minX, maxX] = d3.extent(points, (p) => p.x) as [number, number];
+  const x = d3.scaleLinear([minX, maxX], [marginLeft, width - marginRight]);
+  const [minY, maxY] = d3.extent(points, (p) => p.y) as [number, number];
   const y = d3.scaleLinear([minY, maxY], [height - marginBottom, marginTop]);
-  const line = d3.line((_, i) => x(i), y);
 
-  // TODO: Resolve those ts-ignores
+  const gx = useRef(null);
+  const gy = useRef(null);
   // @ts-ignore
   useEffect(() => void d3.select(gx.current).call(d3.axisBottom(x)), [gx, x]);
   // @ts-ignore
@@ -45,15 +64,9 @@ function LinePlot({
     <svg width={width} height={height}>
       <g ref={gx} transform={`translate(0,${height - marginBottom})`} />
       <g ref={gy} transform={`translate(${marginLeft},0)`} />
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        d={line(data) || undefined}
-      />
       <g fill="white" stroke="currentColor" strokeWidth="1.5">
-        {data.map((d, i) => (
-          <circle key={i} cx={x(i)} cy={y(d)} r="2.5" />
+        {points.map((p, i) => (
+          <circle key={i} cx={x(p.x)} cy={y(p.y)} r="2.5" />
         ))}
       </g>
     </svg>
@@ -61,18 +74,24 @@ function LinePlot({
 }
 
 export default function Page() {
-  const [data, setData] = useState<Array<number>>([1, 2, 3]);
+  const [points, setData] = useState<Array<Point>>([]);
+  const scatterPlotContainer = useRef(null);
 
   function onSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const num = Number(formData.get("number"));
-    setData((d) => [...d, num]);
+
+    const x = Number(formData.get("x"));
+    const y = Number(formData.get("y"));
+    const p: Point = { x, y };
+    setData((ps) => [...ps, p]);
   }
 
   const deleteElem = (i: number) => {
     setData((d) => [...d.slice(0, i), ...d.slice(i + 1)]);
   };
+
+  const { width, height } = useDimensions(scatterPlotContainer);
 
   return (
     <div className="flex flex-col justify-center items-center">
@@ -80,18 +99,29 @@ export default function Page() {
         <input
           className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-black border-black dark:bg-black dark:text-slate-200 dark:border-slate-200 border rounded-md px-2 py-1 outline-none"
           type="number"
-          name="number"
+          name="x"
           step="any"
-          min={0}
-          placeholder="Add number"
+          placeholder="x"
         />
+        <input
+          className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-black border-black dark:bg-black dark:text-slate-200 dark:border-slate-200 border rounded-md px-2 py-1 outline-none"
+          type="number"
+          name="y"
+          step="any"
+          placeholder="y"
+        />
+        <input type="submit" className="hidden" />
       </form>
-      <LinePlot data={data} />
+
+      <div ref={scatterPlotContainer} className="w-full h-[50dvh]">
+        <ScatterPlot points={points} width={width} height={height} />
+      </div>
+
       <table className="mt-5">
         <tbody>
-          {data.map((d, i) => (
-            <tr key={`${i}-${d}`}>
-              <td className="p-2">{d}</td>
+          {points.map((p, i) => (
+            <tr key={`${i}-${p}`}>
+              <td className="p-2">{`(${p.x}, ${p.y})`}</td>
               <td className="p-2">
                 <button
                   onClick={() => deleteElem(i)}
