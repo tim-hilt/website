18cbd8c872a81b6e2804ffb0bc39d2c06ffdc724
Tim Hilt <timhilt@live.de>
2024-01-02 15:11:43 +0100

    Refactor NavBar to use Radix-UI
    ...will come in handy for showing a popover with Tools later

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export function NavBar() {
+export function LegacyNavBar() {
   const pathname = usePathname();
   const entries = [
     {
@@ -15,6 +16,7 @@ export function NavBar() {
       title: "About",
     },
   ];
+
   return (
     <nav className="flex pt-8 md:px-8 px-4 pb-4 text-xl">
       <div className="grow">
@@ -41,8 +43,60 @@ export function NavBar() {
             </div>
           );
         })}
-        {/** TODO: Add tools / projects / explorations */}
+        <div
+          key="tools"
+          className="flex w-[4em] justify-center transition duration-100 hover:scale-105 font-light"
+        >
+          <Link href={"/tools"}>Tools</Link>
+        </div>
       </div>
     </nav>
+  );
+}
+
+export default function NavBar() {
+  const pathname = usePathname();
+  return (
+    <NavigationMenu.Root className="pt-8 md:px-8 px-4 pb-4 text-xl">
+      <NavigationMenu.List className="flex">
+        <NavigationMenu.Item className="grow">
+          <div className="flex">
+            <Link
+              href="/"
+              className="font-semibold transition duration-100 hover:scale-105"
+            >
+              Tim Hilt
+            </Link>
+          </div>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <div
+            className={`flex w-16 transition duration-100 hover:scale-105 ${
+              pathname === "/blog" ? "font-medium" : "font-light"
+            }`}
+          >
+            <Link href="/blog">Blog</Link>
+          </div>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <div
+            className={`flex w-16 transition duration-100 hover:scale-105 ${
+              pathname === "/tools" ? "font-medium" : "font-light"
+            }`}
+          >
+            <Link href="/tools">Tools</Link>
+          </div>
+        </NavigationMenu.Item>
+        <NavigationMenu.Item>
+          <div
+            className={`flex w-16 transition duration-100 hover:scale-105 ${
+              pathname === "/about" ? "font-medium" : "font-light"
+            }`}
+          >
+            <Link href="/about">About</Link>
+          </div>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+    </NavigationMenu.Root>
   );
 }
